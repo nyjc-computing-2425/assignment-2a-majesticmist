41fa4aa6564858ad2d4f930e7280b12914934581
Steve Nguyen <154491875+majesticmist@users.noreply.github.com>
2024-02-29 07:50:28 +0000

    Final Solution

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 num = input('Enter a number (decimal or integer): ')
 # type your code here
 
+new_num = num.strip()
+new_num = new_num.replace('.', '')
+new_num = new_num.lstrip('0')
+
+
+sf = len(new_num)
 
 
 # do not change any code beyond this point
